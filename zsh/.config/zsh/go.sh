ec88b0bc45cf417bc0b95fe63ed93e2e0cc6da6a
Josh Lineaweaver <JLineaweaver@gmail.com>
2021-04-27 21:15:09 -0600

    Update gitignore and no gomod

--- zsh/.config/zsh/go.sh
+++ zsh/.config/zsh/go.sh
@@ -4,4 +4,4 @@ export GOPATH="$HOME/go"
 export PATH=$PATH:$GOPATH/bin
 export GOPRIVATE="github.com/sendgrid/*,github.com/sendgrid-ops/*,github.com/sendgrid-dev/*"
 export GOSUMDB=off
-
+export GO111MODULE=off
