7a4df01cde9b86c90078ea00e777f05e78f2fb77
Josh Lineaweaver <josh.lineaweaver@datadoghq.com>
2025-11-24 09:58:12 -0800

    Neovim checkin

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -7,55 +7,55 @@ return { -- LSP Configuration & Plugins
 			'williamboman/mason-lspconfig.nvim',
 		},
 		config = function()
-			-- LSP settings.
-			local on_attach = function(_, bufnr)
-				local nmap = function(keys, func, desc)
-					if desc then
-						desc = 'LSP: ' .. desc
+			-- LSP settings using LspAttach autocmd (new in nvim 0.11+)
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+				callback = function(event)
+					local bufnr = event.buf
+					local nmap = function(keys, func, desc)
+						if desc then
+							desc = 'LSP: ' .. desc
+						end
+						vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 					end
 
-					vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-				end
+					nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+					nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-				nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-				nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+					nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+					nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+					nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+					nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+					nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols,
+						'[D]ocument [S]ymbols')
+					nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+						'[W]orkspace [S]ymbols')
 
-				nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-				nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-				nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-				nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-				nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols,
-					'[D]ocument [S]ymbols')
-				nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-					'[W]orkspace [S]ymbols')
+					-- See `:help K` for why this keymap
+					nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+					-- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-				-- See `:help K` for why this keymap
-				nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-				-- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+					-- Lesser used LSP functionality
+					nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+					nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+					nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder,
+						'[W]orkspace [R]emove Folder')
+					nmap('<leader>wl', function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, '[W]orkspace [L]ist Folders')
 
-				-- Lesser used LSP functionality
-				nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-				nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-				nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-				nmap('<leader>wl', function()
-					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, '[W]orkspace [L]ist Folders')
+					-- Diagnostic keymaps
+					nmap('[d', vim.diagnostic.goto_prev)
+					nmap(']d', vim.diagnostic.goto_next)
+					nmap('<leader>e', vim.diagnostic.open_float)
+					nmap('<leader>q', vim.diagnostic.setloclist)
 
-				-- Diagnostic keymaps
-				nmap('[d', vim.diagnostic.goto_prev)
-				nmap(']d', vim.diagnostic.goto_next)
-				nmap('<leader>e', vim.diagnostic.open_float)
-				nmap('<leader>q', vim.diagnostic.setloclist)
-
-				-- Create a command `:Format` local to the LSP buffer
-				vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-					if vim.lsp.buf.format then
+					-- Create a command `:Format` local to the LSP buffer
+					vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
 						vim.lsp.buf.format()
-					elseif vim.lsp.buf.formatting then
-						vim.lsp.buf.formatting()
-					end
-				end, { desc = 'Format current buffer with LSP' })
-			end
+					end, { desc = 'Format current buffer with LSP' })
+				end,
+			})
 
 			-- Setup mason so it can manage external tooling
 			require('mason').setup()
@@ -84,21 +84,22 @@ return { -- LSP Configuration & Plugins
 				lineFoldingOnly = true
 			}
 
+			-- Configure LSPs using new vim.lsp.config API
 			for _, lsp in ipairs(defaults) do
-				require('lspconfig')[lsp].setup {
-					on_attach = on_attach,
-					capabilities = capabilities,
-				}
+				if lsp ~= 'gopls' then -- Skip gopls, we'll configure it separately
+					vim.lsp.config(lsp, {
+						capabilities = capabilities,
+					})
+					vim.lsp.enable(lsp)
+				end
 			end
 
-			-- Go  custom config
-			local util = require "lspconfig/util"
-			require('lspconfig').gopls.setup {
-				on_attach = on_attach,
+			-- Go custom config using new API
+			vim.lsp.config.gopls = {
 				capabilities = capabilities,
 				cmd = { "gopls", "serve" },
 				filetypes = { "go", "gomod" },
-				root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+				root_markers = { "go.work", "go.mod", ".git" },
 				settings = {
 					gopls = {
 						analyses = {
@@ -136,6 +137,7 @@ return { -- LSP Configuration & Plugins
 					},
 				},
 			}
+			vim.lsp.enable('gopls')
 		end,
 	},
 	{
